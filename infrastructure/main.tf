34e81a8857841e8a3ac5e37e0a001c0e233bcc07
OseenM <oseenmarcelino.6@gmail.com>
2025-03-07 20:23:24 -0500

    error fixed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  features {  }
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacct"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
